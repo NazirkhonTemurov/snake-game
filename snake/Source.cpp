2782412b7b83b9d33d4a0c91090ad9b8af6c3358
NazirkhonTemurov <93658192+NazirkhonTemurov@users.noreply.github.com>
2021-12-18 17:35:48 +0300

    Поработал над стильем

--- snake/Source.cpp
+++ snake/Source.cpp
@@ -15,93 +15,87 @@ enum eDirection { stop = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
 void setup() {
-	gameOver = false;
+    gameOver = false;
 	dir = stop;
 	x = widht / 2 -1;
 	y = height / 2 -1;
 	fruitx = rand() % widht;
 	fruity = rand() % height;
 	score = 0;
-
 }
 
 void draw() {
-	system("cls");
+    system("cls");
 	for (int i = 0; i < widht+1; ++i) {
-		cout << "#" ;
+cout << "#" ;
 	}
-	cout << endl;
-	for (int i = 0; i < height; ++i) {
-		for (int j = 0; j < widht; ++j) {
-			if (j == 0 || j == widht - 1)
-				cout << "#";
-			if (i == y && j == x) {
-				cout << "0";
-			}
-			else if (i == fruity && j == fruitx) {
-				cout << "F";
-			}else
 
-				cout << " ";
-		
-	
-			
-		}
-		cout << endl;
+cout << endl;
+	for (int i = 0; i < height; ++i) {
+	  for (int j = 0; j < widht; ++j) {
+	    if (j == 0 || j == widht - 1)
+cout << "#";
+          if (i == y && j == x) {
+cout << "0";
+		  } else if (i == fruity && j == fruitx) {
+cout << "F";
+              }else
+cout << " ";	
+      }
+cout << endl;
 	}
 	
-	for (int i = 0; i < widht+1; ++i) {
-		cout << "#";
+    for (int i = 0; i < widht+1; ++i) {
+cout << "#";
 	}
-	cout << endl;
-	cout << "Score: " << score << endl;
+cout << endl;
+cout << "Score: " << score << endl;
 }
 
 void input() {
 	if (_kbhit()) {
-		switch (_getch()) {
-		case'a':
-			dir = LEFT;
-		case'd':
-			dir = RIGHT;
-		case'w':
-			dir = UP;
-		case's':
-			dir = DOWN;
-		case'x':
-		    gameOver = true;
-		}
+      switch (_getch()) {
+	  case'a':
+	    dir = LEFT;
+	  case'd':
+	    dir = RIGHT;
+	  case'w':
+		dir = UP;
+	  case's':
+		dir = DOWN;
+	  case'x':
+	    gameOver = true;
+	  }
 	}
 }
 
 void logic() {
-	switch (dir){
-	case stop:
-		break;
-	case LEFT:
-		--x;
-		break;
-	case RIGHT:
-		++x;
-		break;
-	case UP:
-		--y;
-		break;
-	case DOWN:
-		++x;
-		break;
+    switch (dir){
+	  case stop:
+	    break;
+	  case LEFT:
+	    --x;
+	    break;
+	  case RIGHT:
+	    ++x;
+	   break;
+	  case UP:
+	    --y;
+	   break;
+	  case DOWN:
+	    ++x;
+	    break;
 	}
 
 	if (x > widht || x < 0 || y > height || y < 0) {
-		gameOver = true;
+	  gameOver = true;
 	}
 
 	if (x == fruitx && y == fruity) {
-		score += 10;
-		fruitx = rand() % widht;
-		fruity = rand() % height;
+	  score += 10;
+	  fruitx = rand() % widht;
+	  fruity = rand() % height;
 	}
-
 }
 
 int main(){
@@ -111,7 +105,4 @@ int main(){
       input();
       logic();
 }
-
-return 0;
-
-}
+    }
